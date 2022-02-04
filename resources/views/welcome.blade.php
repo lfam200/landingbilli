ef63438851893b9b5278fa08fb7d7721d330180d
Luis Abanto Moreno <lfam200@hotmail.com>
2022-02-04 18:53:33 -0500

    Fix logo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,11 +12,14 @@
 
 <!-- Section 1 -->
 <section class="w-full px-8 py-16 bg-yellow-400 xl:px-8">
+    <div class="flex md:hidden justify-center">
+        <img src="{{ secure_asset('img/logo_billi.png')}}" alt="" class="w-32">
+    </div>
     <div class="max-w-5xl mx-auto">
         <div class="flex flex-col items-center md:flex-row">
             @livewire('form-contact')
-            <div class="w-full space-y-5 md:w-3/5 md:pl-16">
-              <div class="flex justify-center">
+            <div class="w-full space-y-5 md:w-3/5 md:pl-16 ">
+              <div class="hidden md:flex justify-center">
                 <img src="{{ secure_asset('img/logo_billi.png')}}" alt="" class="w-16 md:w-32 lg:w-48 object-center">
               </div>
 
