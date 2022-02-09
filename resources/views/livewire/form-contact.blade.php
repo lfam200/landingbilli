9cc91a47fe2a2549076341d8fab4a445eadd9b9f
Luis Abanto Moreno <lfam200@hotmail.com>
2022-02-09 00:14:45 -0500

    Fix loader button

--- resources/views/livewire/form-contact.blade.php
+++ resources/views/livewire/form-contact.blade.php
@@ -40,16 +40,18 @@
           <button
               class="w-full px-3 py-4 font-medium text-white bg-yellow-400 rounded-lg"
               wire:click.prevent='sendEmail'
-
-              wire:target="sendEmail"
+              wire:loading.attr="disabled"
           >
-              <div class="flex items-center justify-center" wire:loading>
-                  <svg class="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                      <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
-                      <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-                  </svg>
-                  Enviando...
+              <div wire:loading wire:target="sendEmail">
+                  <div class="flex items-center justify-center" >
+                      <svg class="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                          <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                          <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                      </svg>
+                      Enviando...
+                  </div>
               </div>
+
               <div wire:loading.remove>
                   Enviar
               </div>
